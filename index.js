1487ddf35eb621b2156ab5eca3cf06a49c211633
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-12-14 16:02:35 +0100

    it work, todo image not found function

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ console.log("movie db")
 
 // 379cd6130723c7c61a78ce00569516c5
 // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzljZDYxMzA3MjNjN2M2MWE3OGNlMDA1Njk1MTZjNSIsInN1YiI6IjY1N2FlMTIxN2EzYzUyMDEwY2RmOWFiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5CMWVNczbg37OUH7kmPMoRvuNiNe4MK44IyWSdXI7TQ
-
+// https://aeroclub-issoire.fr/wp-content/uploads/2020/05/image-not-found.jpg
 // console.log("movie db");
 
 const main = document.querySelector('main')
+let pageIndex;
 let totalResult;
-let currentPage = 1;
 let moviesList = [];
-
+let movieName = "batman";
 
 const indexesContainer = document.querySelector('.indexes-container');
 
@@ -20,10 +20,16 @@ const indexPagesDisplayer = (data) => {
     indexesContainer.innerHTML = "";
     for (let i = 1; i < numberOfPages + 1; i++) {
         indexesContainer.innerHTML += `
-        <span class="pages-index" id="${i}">${i}</span>
+        <span class="page-index" id="${i}">${i}</span>
         `
+        pageIndex = document.querySelectorAll('.page-index');
     }
-
+    pageIndex.forEach((index) => {
+        index.addEventListener('click', () => {
+            // console.log(index.id)
+            movieNameFetcher(movieName, index.id)
+        });
+    });
 }
 
 const moviesDisplayer = (movies, img) => {
@@ -32,11 +38,12 @@ const moviesDisplayer = (movies, img) => {
     main.innerHTML = "";
 
     for (let i = 0; i < movies.length; i++) {
+
         main.innerHTML +=
             `
         <div class="movie-container">
             <div class="img-movie">
-                <img src="doc/batman-img.png" alt="image du film ${movies[i].title}">
+                <img src="https://image.tmdb.org/t/p/original/${movies[i].poster_path}" alt="image du film ${movies[i].title}">
             </div>
             <div class="title-movie-container">
                 <h3>${movies[i].title}</h3>
@@ -49,6 +56,7 @@ const moviesDisplayer = (movies, img) => {
         `
     }
 }
+
 
 const movieNameFetcher = async (movie, page) => {
     const options = {
@@ -75,23 +83,15 @@ const movieNameFetcher = async (movie, page) => {
 
 
 window.addEventListener('load', () => {
-    movieNameFetcher('batmAn', currentPage)
+    movieNameFetcher('batmAn', 1)
 });
 
+document.getElementById('search-btn').addEventListener('click', (e) => {
+    e.preventDefault()
+    if (document.getElementById('search-text').value.length > 0) {
+        movieNameFetcher(document.getElementById('search-text').value, 1)
+    } else {
+        alert("Entrez un nom de film")
+    }
+});
 
-// const moviesImagesFetcher = async (id) => {
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             accept: 'application/json',
-//             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzljZDYxMzA3MjNjN2M2MWE3OGNlMDA1Njk1MTZjNSIsInN1YiI6IjY1N2FlMTIxN2EzYzUyMDEwY2RmOWFiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5CMWVNczbg37OUH7kmPMoRvuNiNe4MK44IyWSdXI7TQ'
-//         }
-//     };
-//     await fetch('https://api.themoviedb.org/3/movie/' + 268 + '/images', options)
-//         .then(response => response.json())
-//         .then(data => console.log(data))
-//         .catch(err => console.error(err));
-
-// }
-
-// moviesImagesFetcher(870358)
