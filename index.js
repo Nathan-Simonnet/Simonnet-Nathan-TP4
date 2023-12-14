951178d04398ab4ebd8cb64d6dd016fe192d5eb0
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-12-14 16:27:13 +0100

    done, css plus factoring todo

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-console.log("movie db")
-
 // 379cd6130723c7c61a78ce00569516c5
 // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzljZDYxMzA3MjNjN2M2MWE3OGNlMDA1Njk1MTZjNSIsInN1YiI6IjY1N2FlMTIxN2EzYzUyMDEwY2RmOWFiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5CMWVNczbg37OUH7kmPMoRvuNiNe4MK44IyWSdXI7TQ
-// https://aeroclub-issoire.fr/wp-content/uploads/2020/05/image-not-found.jpg
-// console.log("movie db");
 
 const main = document.querySelector('main')
 let pageIndex;
 let totalResult;
 let moviesList = [];
-let movieName = "batman";
+let movieName = "";
+let notFound = 'https://aeroclub-issoire.fr/wp-content/uploads/2020/05/image-not-found.jpg'
 
 const indexesContainer = document.querySelector('.indexes-container');
 
 const indexPagesDisplayer = (data) => {
-    const numberOfPages = Math.floor(data / 20)
-    console.log(numberOfPages)
+
+    data > 200 ? data = 200 : data;
+    let numberOfPages = Math.floor(data / 20)
+    // console.log(numberOfPages)
+
+
 
     indexesContainer.innerHTML = "";
     for (let i = 1; i < numberOfPages + 1; i++) {
@@ -32,18 +33,23 @@ const indexPagesDisplayer = (data) => {
     });
 }
 
-const moviesDisplayer = (movies, img) => {
+const moviesDisplayer = (movies) => {
     console.log(movies)
-
+    let img;
     main.innerHTML = "";
 
     for (let i = 0; i < movies.length; i++) {
 
+        if (movies[i].poster_path == null) {
+            img = notFound
+        } else {
+            img = `https://image.tmdb.org/t/p/original/${movies[i].poster_path}`
+        }
         main.innerHTML +=
             `
         <div class="movie-container">
             <div class="img-movie">
-                <img src="https://image.tmdb.org/t/p/original/${movies[i].poster_path}" alt="image du film ${movies[i].title}">
+                <img src="${img}" alt="image du film ${movies[i].title}">
             </div>
             <div class="title-movie-container">
                 <h3>${movies[i].title}</h3>
@@ -52,7 +58,6 @@ const moviesDisplayer = (movies, img) => {
                 <p>${movies[i].overview}</p>
             </div>
         </div>
-        
         `
     }
 }
@@ -70,9 +75,9 @@ const movieNameFetcher = async (movie, page) => {
     await fetch('https://api.themoviedb.org/3/search/movie?&query=' + movie + "&page=" + page, options)
         .then(response => response.json())
         .then(data => {
-            moviesList = data.results
+            moviesList = data.results.slice(0, 200)
             totalResult = data.total_results
-            console.log(data);
+            console.log(moviesList);
         })
         .catch(err => console.error(err));
 
@@ -90,6 +95,7 @@ document.getElementById('search-btn').addEventListener('click', (e) => {
     e.preventDefault()
     if (document.getElementById('search-text').value.length > 0) {
         movieNameFetcher(document.getElementById('search-text').value, 1)
+        movieName = document.getElementById('search-text').value;
     } else {
         alert("Entrez un nom de film")
     }
